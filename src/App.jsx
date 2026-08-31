b2b00c211aaa18ababe68f4a85aa35933cefdc2c
Anushka Sarkar <sarkaranushka614@gmail.com>
2026-08-31 12:07:58 +0530

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ function App() {
 
   return (
     <>
-          <h1>Hello world</h1>
+      <h1 className="bg-blue-500 text-white p-4">Hello world</h1>
+      <button className="btn btn-primary">Test Button</button>
     </>
   )
 };
