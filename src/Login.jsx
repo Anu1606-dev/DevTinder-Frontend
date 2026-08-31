25afb6a1d1817f94c450aab3762faaf1b639d9af
Anushka Sarkar <sarkaranushka614@gmail.com>
2026-08-31 16:12:59 +0530

    feat: enhance Login component with structured layout and styling

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,7 +1,19 @@
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className="flex justify-center items-center h-screen bg-base-100">
+      <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-96 border p-6 gap-2">
+        <legend className="fieldset-legend text-xl font-bold px-2">Login</legend>
+
+        <label className="label">Email</label>
+        <input type="email" className="input w-full" placeholder="Email" />
+
+        <label className="label">Password</label>
+        <input type="password" className="input w-full" placeholder="Password" />
+
+        <button className="btn btn-neutral w-full mt-4">Login</button>
+      </fieldset>
+    </div>
   )
 }
 
-export default Login
+export default Login;
