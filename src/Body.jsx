35f6fa099bff0c146c9e3cf3278d0f6a8482a657
Anushka Sarkar <sarkaranushka614@gmail.com>
2026-08-31 13:13:16 +0530

    feat: add Footer component and integrate it into Body

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import NavBar from './NavBar';
+import Footer from './Footer';
 
 const Body = () => {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
         <NavBar />
         <Outlet />
+        <Footer />
     </div>
   )
 }
