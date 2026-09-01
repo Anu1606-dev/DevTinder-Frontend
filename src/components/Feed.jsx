93863d2ca6692d8d921316c69fa3450b7e1c1baa
Anushka Sarkar <sarkaranushka614@gmail.com>
2026-09-01 14:04:13 +0530

    feat: implement feed management with Redux and create feedSlice for state handling

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,32 @@
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch, useSelector, useEffect } from 'react-redux';
+import { addFeed } from "../utils/feedSlice";
 
 const Feed = () => {
+  const feed = useSelector((store) => store.feed);
+  const dispatch = useDispatch();
+
+  const getFeed = async () => {
+    if (feed) return;
+    try {
+      const res = await axios.get(BASE_URL + "/feed", {
+        withCredentials: true,
+      });
+      dispatch(addFeed(res.data));
+    } catch (error) {
+      console.error("Error fetching feed:", error);
+    }
+  };
+
+  useEffect(() => {
+    getFeed();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div>Feed</div>
   )
 }
 
-export default Feed
+export default Feed;
